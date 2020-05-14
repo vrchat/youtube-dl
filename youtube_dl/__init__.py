2d5e4b3d0c0b75b875c74ddd2567136dd8aa23f7
MehStrongBadMeh <mehstrongbadmeh+github@gmail.com>
2020-05-13 20:10:49 -0400

    Update __init__.py

--- youtube_dl/__init__.py
+++ youtube_dl/__init__.py
@@ -461,7 +461,7 @@ def _real_main(argv=None):
 
         # Maybe do nothing
         if (len(all_urls) < 1) and (opts.load_info_filename is None):
-            if '''opts.update_self or''' opts.rm_cachedir:
+            if opts.rm_cachedir:
                 sys.exit()
 
             ydl.warn_if_short_id(sys.argv[1:] if argv is None else argv)
